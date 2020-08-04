755cc871854d9781fc9c224acfcc2ccc709a24bd
marxsv225 <marcelvaka@gmail.com>
2020-08-04 11:18:06 +0200

    Updates

---
+++ precache-manifest.ec37c644141bc8b4d288ec13a159efea.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f324b3e9eeaf94746aded301ee6bf39",
+    "url": "/reactProject/index.html"
+  },
+  {
+    "revision": "fe96bef0f188ee34a919",
+    "url": "/reactProject/static/css/2.e6c921eb.chunk.css"
+  },
+  {
+    "revision": "b80c5875232f489bece0",
+    "url": "/reactProject/static/css/main.0b0c8b4a.chunk.css"
+  },
+  {
+    "revision": "fe96bef0f188ee34a919",
+    "url": "/reactProject/static/js/2.ded6e0e6.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/reactProject/static/js/2.ded6e0e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b80c5875232f489bece0",
+    "url": "/reactProject/static/js/main.80109099.chunk.js"
+  },
+  {
+    "revision": "6a3b7761a013021e6524",
+    "url": "/reactProject/static/js/runtime-main.a91a0920.js"
+  },
+  {
+    "revision": "f1fdf59f696f9b789da5e1c8b28d3ebe",
+    "url": "/reactProject/static/media/arrow-down-mint.f1fdf59f.svg"
+  },
+  {
+    "revision": "8fe18a6e219b84babec7b6aa1ca7d461",
+    "url": "/reactProject/static/media/bgc.8fe18a6e.png"
+  },
+  {
+    "revision": "49e94f637e873d3405e6678a75a98115",
+    "url": "/reactProject/static/media/bgimg.49e94f63.jpg"
+  },
+  {
+    "revision": "56b7eaa7eebf5c558447e7c0d916b7a7",
+    "url": "/reactProject/static/media/bgnew.56b7eaa7.PNG"
+  },
+  {
+    "revision": "3d24fec82fea122c500c9aa876684250",
+    "url": "/reactProject/static/media/mission.3d24fec8.jpg"
+  }
+]);
